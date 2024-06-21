0eae90e4e0aefea2f6bf6add9485ba959568a9b1
anis safia <anissafia110@gmail.com>
2024-06-21 15:40:20 +0100

    Remove Duplicate Code

--- src/js/script.js
+++ src/js/script.js
@@ -23,16 +23,6 @@ const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]'
 );
 
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
-
 //  Store color theme for future visits
 
 function switchTheme(e) {
@@ -44,6 +34,8 @@ function switchTheme(e) {
     localStorage.setItem("theme", "light"); //add this
   }
 }
+
+toggleSwitch.addEventListener("change", switchTheme, false);
 
 // Save user preference on load
 
